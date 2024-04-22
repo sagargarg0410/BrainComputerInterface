f26b668074f8c1dba222fc08e740aec2c0b996f7
Sagar Garg <sagargarg853@gmail.com>
2024-04-22 23:29:21 +0530

    Thode se changes

--- Website/src/components/navbar/navbar.jsx
+++ Website/src/components/navbar/navbar.jsx
@@ -4,20 +4,20 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css'
 import { useState } from 'react';
 
-const Menu=()=>{
-  return(
-  <>
-  
-  <p><a href="/home">Home</a></p>
-  <p><a href="/prosthetic">Prosthetic</a></p>
-  {/* <p><a href="/demo">Demonstration</a></p>   */}
-  <p><a href="/Games">Mini Games</a></p>
-  {/* <li><link to={"#Games"}>Mini Games</link></li> */}
-  {/* <p><a href="#blog">Library</a></p> */}
-  <p><a href="/Sentient"> Sentient Analysis</a></p>
-  <p><a href="/ImageComponent">Facial Expressions</a></p>
-  <p><a href="/Maker">Founders</a></p>
-  </>)
+const Menu = () => {
+  return (
+    <>
+
+      <p><a href="/home">Home</a></p>
+      <p><a href="/prosthetic">BCI</a></p>
+      {/* <p><a href="/demo">Demonstration</a></p>   */}
+      <p><a href="/Games">Mini Games</a></p>
+      {/* <li><link to={"#Games"}>Mini Games</link></li> */}
+      {/* <p><a href="#blog">Library</a></p> */}
+      <p><a href="/Sentient"> Sentient Analysis</a></p>
+      <p><a href="/ImageComponent">Facial Expressions</a></p>
+      <p><a href="/Maker">Founders</a></p>
+    </>)
 }
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -26,21 +26,21 @@ const Navbar = () => {
       <div className='gpt__navbar-links'>
         <div className='gpt__navbar-links_logo'>
           {/* <img src={logo} alt="logo" /> */}
-         </div>
-        <div className='gpt__navbar-links_container'><Menu/> 
         </div>
+        <div className='gpt__navbar-links_container'><Menu />
         </div>
-        <div className='gpt__navbar-sign'>
-          {/* <p>Sign in</p> */}
-          {/* <button type='button'>Sign Up</button> */}
-        </div>
-        <div className='gpt__navbar-menu'>
+      </div>
+      <div className='gpt__navbar-sign'>
+        {/* <p>Sign in</p> */}
+        {/* <button type='button'>Sign Up</button> */}
+      </div>
+      <div className='gpt__navbar-menu'>
         {toggleMenu
           ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
           : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
         {toggleMenu && (
-        <div className="gpt__navbar-menu_container scale-up-center">
-          {/* <div className="gpt__navbar-menu_container-links">
+          <div className="gpt__navbar-menu_container scale-up-center">
+            {/* <div className="gpt__navbar-menu_container-links">
             <p><a href="#home">Home</a></p>
             <p><a href="#whatgpt">What is GPT3??</a></p>
             <p><a href="#possibilities">Open AI</a></p>
@@ -48,15 +48,15 @@ const Navbar = () => {
             <p><a href="#blog">Library</a></p>
             <Menu/>
           </div> */}
-          <div className="gpt__navbar-menu_container-links-sign">
-            <p>Sign in</p>
-            <button type="button">Sign up</button>
+            <div className="gpt__navbar-menu_container-links-sign">
+              <p>Sign in</p>
+              <button type="button">Sign up</button>
+            </div>
           </div>
-        </div>
         )}
-        </div>
-      
-    
+      </div>
+
+
     </div>
   )
 }
--- Website/src/pages/Makers/maker.jsx
+++ Website/src/pages/Makers/maker.jsx
@@ -5,67 +5,70 @@ import joyeeta from '../../assets/joyeeta.jpeg'
 import anshu from '../../assets/anshu.jpeg'
 import jatin from '../../assets/jatin.jpg'
 
-const Maker=()=>{
-    return(
+const Maker = () => {
+    return (
         <div className="container">
             <div className="card">
                 <div className="imgbox">
-                   <img src={Sagar2} alt="s"></img>
-                   <br></br>
-                   <h1 className="gradient__text">Sagar Garg</h1>
+                    <img src={Sagar2} alt="s"></img>
+                    <br></br>
+                    <h1 className="gradient__text">Sagar Garg</h1>
                 </div>
                 <div className="content">
                     <p>Sagar Garg is currently pursuing
-his bachelor’s degree in ECE
-from BVCOE, GGSIPU, New
-Delhi. He has deep fascination for
-Embedded Systems. He has worked
-on many automation projects that
-includes portable small form factor
-function generator, music mapped
-LEDS to name a few.</p>
+                        his bachelor’s degree in ECE
+                        from BVCOE, GGSIPU, New
+                        Delhi. He has deep fascination for
+                        Embedded Systems. He has worked
+                        on many automation projects that
+                        includes portable small form factor
+                        function generator, music mapped
+                        LEDS to name a few.</p>
                 </div>
             </div>
             <div className="card">
                 <div className="imgbox">
-                   <img src={joyeeta} alt="s"></img>
-                   <br></br>
-                   <h1 className="gradient__text">Joyeeta C.</h1>
+                    <img src={joyeeta} alt="s"></img>
+                    <br></br>
+                    <h1 className="gradient__text">Joyeeta Choubey</h1>
                 </div>
                 <div className="content">
-                    <p>Joyeeta Choubey is currently pursuing
-her bachelor’s degree in ECE
-from BVCOE, GGSIPU, New
-Delhi. She is enthusiastic and motivated learner proficient in front-end technologies.  Dedicated to stay up to date with industry trends and emerging technologies .</p>
+                    <p> <br></br>Joyeeta Choubey is currently pursuing
+                        her bachelor’s degree in ECE
+                        from BVCOE, GGSIPU, New
+                        Delhi. She is enthusiastic and motivated learner proficient in front-end technologies.
+                        Dedicated to stay up to date with industry trends and emerging technologies .</p>
                 </div>
             </div>
             <div className="card">
                 <div className="imgbox">
-                   <img src={anshu} alt="s"></img>
-                   <br></br>
-                   <h1 className="gradient__text">Anshu Raj</h1>
+                    <img src={anshu} alt="s"></img>
+                    <br></br>
+                    <h1 className="gradient__text">Anshu Raj</h1>
                 </div>
-                <div className="content"> 
+                <div className="content">
                     <p>Anshu is working as a traniee in Newgen and currently pursuing
-his bachelor’s degree in ECE
-from BVCOE, GGSIPU, New
-Delhi. He is enthusiastic and motivated learner proficient in MERN Stacks. Dedicated to stay up to date with industry trends and emerging technologies.</p>
+                        his bachelor’s degree in ECE
+                        from BVCOE, GGSIPU, New
+                        Delhi. He is enthusiastic and motivated learner proficient in MERN Stacks.
+                        Dedicated to stay up to date with industry trends and emerging technologies.</p>
                 </div>
             </div>
             <div className="card">
                 <div className="imgbox">
-                   <img src={jatin} alt="s"></img>
-                   <br></br>
-                   <h1 className="gradient__text">Jatin Kaushik</h1>
+                    <img src={jatin} alt="s"></img>
+                    <br></br>
+                    <h1 className="gradient__text">Jatin Kaushik</h1>
                 </div>
-                <div className="content"> 
+                <div className="content">
                     <p>Jatin is currently pursuing
-his bachelor’s degree in ECE
-from BVCOE, GGSIPU, New
-Delhi. He is a highly motivated and
-confident undergraduate with a
-strong passion for problem solving. He has hands-on experience on IOT devices and proficient in data structures and algorithms.
-</p>
+                        his bachelor’s degree in ECE
+                        from BVCOE, GGSIPU, New
+                        Delhi. He is a highly motivated and
+                        confident undergraduate with a
+                        strong passion for problem solving.
+                        He has hands-on experience on IOT devices and proficient in data structures and algorithms.
+                    </p>
                 </div>
             </div>
         </div>
